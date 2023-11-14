79b9a742825ac5c83fdb8897f248a375fca0e66d
Javi <70613839+cjavierr@users.noreply.github.com>
2023-11-14 15:56:27 -0700

    Added buttons to home page

--- catport/src/components/pages/home.tsx
+++ catport/src/components/pages/home.tsx
@@ -1,10 +1,42 @@
-//src/components/pages/Home.tsx
 import React from 'react';
+import { Button, Row, Col } from 'antd';
+
 const Home = () => {
   return (
-    <div>
-        <h1 style={{textAlign: 'center'}}>Welcome Ibby! 👋</h1> 
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Welcome Ibby! 👋</h1>
+
+      <Row gutter={[16, 16]} justify="center">
+        <Col span={12}>
+          <Button type="default" block>
+            Create Projects
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="default" block>
+            My Project Listings
+          </Button>
+        </Col>
+
+        <Col span={12}>
+          <Button type="default" block>
+            Manage Risks and Issues
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button type="default" block>
+            Manage Higher Level Action Request
+          </Button>
+        </Col>
+
+        <Col span={12}>
+          <Button type="default" block>
+            Manage Lessons Learned
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 }
+
 export default Home;
