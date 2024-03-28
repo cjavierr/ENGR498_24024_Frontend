24a6c75e2ed56654edac89144347d74b5ef08606
Javi <70613839+cjavierr@users.noreply.github.com>
2024-03-28 16:03:57 -0700

    mock data

--- catport/src/components/pages/dashboard.tsx
+++ catport/src/components/pages/dashboard.tsx
@@ -4,10 +4,18 @@ import { Button} from "antd";
 
 const Dashboard: React.FC = () => {
   const [data1, setData1] = useState([
-    [{ value: "Vanilla" }, { value: "Chocolate" }, { value: "" }],
-    [{ value: "Strawberry" }, { value: "Cookies" }, { value: "" }],
+    [{ value: "48000" }, { value: "16000" }, { value: "32000" }, { value: "16000" }, { value: "64000" }, { value: "16000" }, { value: "48000" }],
+    [{ value: "16000" }, { value: "32000" }, { value: "16000" }, { value: "32000" }, { value: "16000" }, { value: "32000" }, { value: "16000" }],
+    [{ value: "48000" }, { value: "64000" }, { value: "32000" }, { value: "16000" }, { value: "16000" }, { value: "16000" }, { value: "48000" }],
+    [{ value: "32000" }, { value: "48000" }, { value: "16000" }, { value: "16000" }, { value: "32000" }, { value: "32000" }, { value: "32000" }],
+    [{ value: "48000" }, { value: "16000" }, { value: "32000" }, { value: "16000" }, { value: "32000" }, { value: "48000" }, { value: "48000" }],
   ]);
-  const columnLabels = [""]
+  const columnLabels = ["8/1/2023", "9/1/2023", "10/1/2023", "11/1/2023",
+                        "12/1/2023", "1/1/2024", "2/1/2024"]
+
+  const rowLabels = ["Shop Mechanics", "Instrument Techs", "Operators",
+                      "Engineer - Type 1", "Engineer - Type 2"]
+
   const saveData = () => {
     console.log(data1);
     // Here you can add code to save data1 to a server
@@ -15,7 +23,8 @@ const Dashboard: React.FC = () => {
 
   return (
     <div>
-      <Spreadsheet data={data1} onChange={setData1 as any} />
+      <b>Category:</b> Roles
+      <Spreadsheet data={data1} onChange={setData1 as any} columnLabels={columnLabels} rowLabels={rowLabels} />
         <br />
       <Button type="primary" onClick={saveData}>Save</Button>
     </div>
