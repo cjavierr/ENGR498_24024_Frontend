a0422a766bbff98bf909c458dc9ce7cbb1469e03
jerryeagle <jerryeagle@arizona.edu>
2024-05-01 10:58:49 -0700

    admin

--- catport/src/components/pages/Admin.js
+++ catport/src/components/pages/Admin.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import axios from "axios";
 import "../../App.css"; // Import the CSS file
 import { CloseOutlined } from "@ant-design/icons";
@@ -22,8 +21,37 @@ import { useHistory } from "react-router";
 const { Option } = Select;
 const { TextArea } = Input;
 
-function Admin() {
 
-}
+const Admin = () => {
+    const [isAdmin, setIsAdmin] = useState(false);
+
+
+    useEffect(()=>{
+        const username = localStorage.getItem("loggedInUser");
+        const fetchUser= async ()=>{
+            const response = await axios.post("http://localhost:3001/api/getUser", {
+                username: username, // Send username in request body
+            });
+            if(!response.data.isAdmin){
+
+            }else{
+                setIsAdmin(true);
+            }
+        }
+        fetchUser();
+    },[]);
+
+    return(
+        <>
+        {
+            isAdmin ?(
+                <p>Welcome admin</p>
+             ):(
+                <p>Access Denied</p>
+              )
+        }
+        </>
+    );
+};
 
 export default Admin;
